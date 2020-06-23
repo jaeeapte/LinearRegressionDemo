90c282ddec43dabc6a2dc0b68e89dd26c65d0987
Jaee Apte <jaee.apte@gmail.com>
2020-06-23 14:33:22 -0700

    Adding ipynb and data files

---
+++ predictor.py
@@ -0,0 +1,20 @@
+import sys
+import pickle
+import datetime
+import pandas as pd
+
+# load it back in.
+with open('my_linear_regression.sav', 'rb') as pickle_file:
+     regression_model_2 = pickle.load(pickle_file)
+
+# make a new prediction.
+def predict(value):
+    prediction = regression_model_2.predict([[value]])
+    predicted_value = prediction[0][0]
+    print("Predicted value, " , predicted_value)
+
+#arg = sys.argv[1]
+#predict(float(arg))
+
+for arg in sys.argv[1:]:
+  predict(float(arg))
