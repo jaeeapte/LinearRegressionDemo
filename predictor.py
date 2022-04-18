0d962413ba45269851feb8fa478ee0f1acc13880
Jaee Apte <jaee.apte@dominodatalab.com>
2022-04-18 13:29:47 -0700

    Pushed from Domino: https://cloud-develop.domino.tech/workspace/jaee_apte/LinearRegressionDemo-Github?executionId=6191aad84e3e8d1b610133be

--- predictor.py
+++ predictor.py
@@ -4,7 +4,7 @@ import datetime
 import pandas as pd
 
 # load it back in.
-with open('my_linear_regression.sav', 'rb') as pickle_file:
+with open('/mnt/artifacts/my_linear_regression.sav', 'rb') as pickle_file:
      regression_model_2 = pickle.load(pickle_file)
 
 # make a new prediction.
